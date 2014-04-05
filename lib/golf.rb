e56fa691dccbaa3f40b466b514112b46688f3d04
Andrew Ledvina <led@fb.com>
2014-04-04 23:28:50 -0700

    got it under 900

--- lib/golf.rb
+++ lib/golf.rb
@@ -26,40 +26,34 @@ class Golf
 
   def self.hole5(a)
     r,l = [], a.size
-    (0...l).each {|n| (1..l).each {|k| r << a.slice(n,k) }}
+    (0...l).map {|n| (1..l).map {|k| r << a.slice(n,k) }}
     r.uniq.sort_by {|x| [x.size, x[0]]}
   end
 
   def self.hole6(n)
-    (1..n).map{|k| (k%3<1) ? (k%5<1) ? "fizzbuzz" : "fizz" : (k%5<1) ? "buzz" : k}
+    (1..n).map { |i| "#{[:fizz][i%3]}#{[:buzz][i%5]}"[/.+/] || i }
   end
 
   def self.hole7(a)
     r = [a[0]]
-    a.each_cons(2){|x,y| r << x << y if y-x!=1}
+    a.each_cons(2){|x,y| r << x << y if y-x>1}
     r << a[-1] if a[-1] != r[-1]
-    r.each_slice(2).map{|a,b| if b.nil?||a==b then "#{a}" else "#{a}-#{b}" end}
+    r.each_slice(2).map{|a,b| a!=b ? "#{a}-#{b}" : "#{a}"}
   end
 
   def self.hole8(f)
-    (1..f).inject([1,1]) {|a,n| a << a[-1] + a[-2]}.take(f)
+    (1..f-2).inject([1,1]) {|a| a << a[-1] + a[-2]}
   end
 
   def self.hole9(f)
     v = []
-    open(f) { |e|
-      e.each { |l|
-        t = []
-        l.split(", ").each { |a| t << a.chomp }
-        v << t
-      }
-    }
+    open(f) { |e| e.map { |l| v << l.chomp.split(", ") } }
     loop {
       r = v.inject(Hash.new(0)) {|a, n| a[n[0]] += 1; a}
-      r.each { |k, c| return k if c > v.length/2 }
-      z = r.find { |a, b| b == r.values.min}[0]
-      v.map { |e| e.delete(z) }
-      return r.find { |a, b| b == r.values.max}[0] if r.keys.compact.length == 2
+      r.each { |k, c| return k if c > v.size/2 }
+      m,x=r.values.minmax
+      v.map {|e| e.delete(r.find {|a, b| b == m}[0]) }
+      return r.find {|a, b| b == x}[0] if r.keys.compact.size < 3
     }
   end
 end
